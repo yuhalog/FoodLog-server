f3b6c8acee02f333047fdf21d300edd8bf3372b1
YuHayeon <youha0930@gmail.com>
2022-05-04 17:54:28 +0900

    add: 프로젝트 생성

---
+++ src/main/java/dku/capstone/foodlog/FoodLogApplication.java
@@ -0,0 +1,13 @@
+package dku.capstone.foodlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dku/capstone/foodlog/FoodLogApplicationTests.java
@@ -0,0 +1,13 @@
+package dku.capstone.foodlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

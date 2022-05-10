d2154e45b031739673a05e035bf87ee4309cb834
YuHayeon <youha0930@gmail.com>
2022-05-11 02:07:40 +0900

    add: MemberService 추가

---
+++ src/main/java/dku/capstone/foodlog/service/MemberService.java
@@ -0,0 +1,9 @@
+package dku.capstone.foodlog.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@Service
+public class MemberService {
+}

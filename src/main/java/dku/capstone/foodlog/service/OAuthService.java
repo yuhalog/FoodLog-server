28323abc515dc332b82d10f52c19f81b58f46e83
YuHayeon <youha0930@gmail.com>
2022-05-14 03:09:15 +0900

    feat: OAuthService 회원가입

--- src/main/java/dku/capstone/foodlog/service/OAuthService.java
+++ src/main/java/dku/capstone/foodlog/service/OAuthService.java
@@ -1,26 +1,30 @@
 package dku.capstone.foodlog.service;
 
+import dku.capstone.foodlog.domain.Member;
 import dku.capstone.foodlog.dto.request.GoogleOAuthToken;
 import dku.capstone.foodlog.dto.response.GoogleUserDto;
+import dku.capstone.foodlog.repository.MemberRepository;
 import dku.capstone.foodlog.utils.GoogleUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class OAuthService {
 
     private final GoogleUtils googleUtils;
     private final HttpServletResponse response;
+    private final MemberRepository memberRepository;
 
     public void request() throws IOException {
 
         String redirectURL = googleUtils.googleRedirectUrl();
-
         response.sendRedirect(redirectURL);
     }
 
@@ -34,4 +38,11 @@ public class OAuthService {
 
         return googleUserDto;
     }
+
+    @Transactional
+    public Long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
+
 }

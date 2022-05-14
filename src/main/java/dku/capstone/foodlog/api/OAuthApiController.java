dd62e9176ea64784203c411427836a98a2472402
YuHayeon <youha0930@gmail.com>
2022-05-14 10:33:21 +0900

    feat: 로그인, 회원가입 api 추가
    이메일이 DB에 있다면 회원가입, 없다면 로그인 진행

--- src/main/java/dku/capstone/foodlog/api/OAuthApiController.java
+++ src/main/java/dku/capstone/foodlog/api/OAuthApiController.java
@@ -1,6 +1,8 @@
 package dku.capstone.foodlog.api;
 
+import dku.capstone.foodlog.domain.Member;
 import dku.capstone.foodlog.dto.response.GoogleUserDto;
+import dku.capstone.foodlog.dto.response.MemberDto;
 import dku.capstone.foodlog.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,10 +23,31 @@ public class OAuthApiController {
     public void googleLoginRedirect() throws IOException {
         oAuthService.request();
     }
+
     @GetMapping("/google/login/redirect")
-    public GoogleUserDto callback(
+    public MemberDto callback(
             @RequestParam(name = "code") String code) throws IOException {
         GoogleUserDto getGoogleUserDto = oAuthService.oAuthLogin(code);
-        return getGoogleUserDto;
+
+        String email = getGoogleUserDto.getEmail();
+        Member findMemberByEmail = oAuthService.findOneByEmail(email);
+
+        if (findMemberByEmail!=null) {
+            // 로그인
+            Long memberId = findMemberByEmail.getId();
+            String jwtToken = null;
+            MemberDto memberDto = new MemberDto(jwtToken, memberId, email);
+
+            return memberDto;
+
+        } else {
+            // 회원가입
+            Member member = new Member(email);
+            Long memberId = oAuthService.join(member);
+            String jwtToken = null;
+            MemberDto memberDto = new MemberDto(jwtToken, memberId, email);
+
+            return memberDto;
+        }
     }
 }

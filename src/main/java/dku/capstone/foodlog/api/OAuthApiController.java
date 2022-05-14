ff6039595e880a1bc0a3be8077a2306fbaafe0d3
YuHayeon <youha0930@gmail.com>
2022-05-14 13:29:10 +0900

    feat: jwt 발급
    회원 인증하기 위해 jwt 발급

--- src/main/java/dku/capstone/foodlog/api/OAuthApiController.java
+++ src/main/java/dku/capstone/foodlog/api/OAuthApiController.java
@@ -4,6 +4,8 @@ import dku.capstone.foodlog.domain.Member;
 import dku.capstone.foodlog.dto.response.GoogleUserDto;
 import dku.capstone.foodlog.dto.response.MemberDto;
 import dku.capstone.foodlog.service.OAuthService;
+import dku.capstone.foodlog.utils.JwtUtils;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +20,15 @@ import java.io.IOException;
 public class OAuthApiController {
 
     private final OAuthService oAuthService;
+    private final JwtUtils jwtUtils;
 
+    @ApiOperation(value = "", notes = "구글 로그인 후 리다이렉트 url 요청")
     @GetMapping("/google/login")
     public void googleLoginRedirect() throws IOException {
         oAuthService.request();
     }
 
+    @ApiOperation(value = "", notes = "DB와 비교해서 로그인 혹은 회원가입 후에 jwt 발급")
     @GetMapping("/google/login/redirect")
     public MemberDto callback(
             @RequestParam(name = "code") String code) throws IOException {
@@ -35,7 +40,7 @@ public class OAuthApiController {
         if (findMemberByEmail!=null) {
             // 로그인
             Long memberId = findMemberByEmail.getId();
-            String jwtToken = null;
+            String jwtToken = jwtUtils.createJwtToken(memberId);
             MemberDto memberDto = new MemberDto(jwtToken, memberId, email);
 
             return memberDto;
@@ -44,7 +49,7 @@ public class OAuthApiController {
             // 회원가입
             Member member = new Member(email);
             Long memberId = oAuthService.join(member);
-            String jwtToken = null;
+            String jwtToken = jwtUtils.createJwtToken(memberId);
             MemberDto memberDto = new MemberDto(jwtToken, memberId, email);
 
             return memberDto;

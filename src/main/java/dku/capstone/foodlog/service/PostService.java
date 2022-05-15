7ecc55b0dce1094630ef16fc7a3ac347cd4bb859
kys0411 <32180846@dankook.ac.kr>
2022-05-15 21:34:53 +0900

    add : PostService 추가

--- src/main/java/dku/capstone/foodlog/service/PostService.java
+++ src/main/java/dku/capstone/foodlog/service/PostService.java
@@ -1,2 +1,18 @@
-package dku.capstone.foodlog.service;public class PostService {
+package dku.capstone.foodlog.service;
+
+import dku.capstone.foodlog.domain.Post;
+import dku.capstone.foodlog.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    public Post savePost(Post post){
+        return postRepository.save(post);
+    }
 }

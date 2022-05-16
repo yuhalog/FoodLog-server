505d910def8a26c4110d868e3160fe431ad95235
YuHayeon <youha0930@gmail.com>
2022-05-16 15:02:19 +0900

    feat: jwt 만료 여부 확인

--- src/main/java/dku/capstone/foodlog/utils/JwtUtils.java
+++ src/main/java/dku/capstone/foodlog/utils/JwtUtils.java
@@ -17,6 +17,11 @@ public class JwtUtils {
     @Value("${jwt.secret-key}")
     private String secretKey;
 
+    /**
+     * 토큰 발급
+     * @param memberId
+     * @return String
+     */
     public String createJwtToken(Long memberId) {
         Date now = new Date();
         Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
@@ -30,16 +35,22 @@ public class JwtUtils {
                 .compact();
     }
 
-
+    /**
+     * 헤더에서 토큰값 가져오기
+     * @return Jwt
+     */
     public String getJwtByHeader() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         return request.getHeader("ACCESS-TOKEN");
     }
 
-    public Claims gwtJwt() {
-        String accessToken = getJwtByHeader();
-        if(accessToken == null || accessToken.length() == 0) {
+    /**
+     * 토큰에서 정보 가져오기
+     * @return
+     */
+    public Claims getClaimByJwt(String token) {
+        if(token == null || token.length() == 0) {
             throw new JwtException("invalid");
         }
 
@@ -48,11 +59,20 @@ public class JwtUtils {
         try{
             claims = Jwts.parser()
                     .setSigningKey(secretKey)
-                    .parseClaimsJws(accessToken);
+                    .parseClaimsJws(token);
         } catch (Exception ignored) {
             throw new JwtException("invalid!");
         }
 
         return claims.getBody();
+    }
+
+    /**
+     * jwt 만료 여부
+     * @param token
+     * @return
+     */
+    public Boolean isJwtExpired(String token) {
+        return getClaimByJwt(token).getExpiration().before(new Date());
     }
 }

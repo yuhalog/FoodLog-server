ae6ca6e43f9dfa12e851dfd5e887a7f229ad6902
YuHayeon <youha0930@gmail.com>
2022-05-16 15:02:19 +0900

    feat: jwt 만료 여부 확인

--- src/main/java/dku/capstone/foodlog/service/JwtService.java
+++ src/main/java/dku/capstone/foodlog/service/JwtService.java
@@ -1,6 +1,7 @@
 package dku.capstone.foodlog.service;
 
 import dku.capstone.foodlog.utils.JwtUtils;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,10 @@ public class JwtService {
     private final JwtUtils jwtUtils;
 
     public Long getIdByJwt() {
-        return jwtUtils.gwtJwt().get("memberId", Long.class);
+        String token = jwtUtils.getJwtByHeader();;
+        if (jwtUtils.isJwtExpired(token)) {
+            throw new JwtException("token expired");
+        }
+        return jwtUtils.getClaimByJwt(token).get("memberId", Long.class);
     }
 }

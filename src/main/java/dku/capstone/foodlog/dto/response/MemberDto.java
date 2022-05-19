82b240ec4a1e457146692fb5027c40d94f0e274a
YuHayeon <youha0930@gmail.com>
2022-05-19 15:33:32 +0900

    feat: 프로필 설정을 위한 프로필 조회 기능

--- src/main/java/dku/capstone/foodlog/dto/response/MemberDto.java
+++ src/main/java/dku/capstone/foodlog/dto/response/MemberDto.java
@@ -1,15 +1,36 @@
 package dku.capstone.foodlog.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dku.capstone.foodlog.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
 @Data
 public class MemberDto {
 
-    private String jwtToken;
+    @JsonProperty(value = "member_id")
     private Long memberId;
     private String email;
+    private String username;
+    private LocalDate birthday;
+
+    @JsonProperty(value = "self_bio")
+    private String selfBio;
+
+    @JsonProperty(value = "profile_picture")
+    private String profilePicture;
+
+    public MemberDto(Member member) {
+        this.memberId = member.getId();
+        this.email = member.getEmail();
+        this.username = member.getUsername();
+        this.birthday = member.getBirthday();
+        this.selfBio = member.getSelfBio();
+        this.profilePicture = member.getProfilePicture();
+    }
 }

813671360c948a6ee6699bddc722bcd1a195a84e
YuHayeon <youha0930@gmail.com>
2022-05-19 12:20:17 +0900

    chore: package 부분 오타 수정

--- src/main/java/dku/capstone/foodlog/config/RestTemplateConfig.java
+++ src/main/java/dku/capstone/foodlog/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-용package dku.capstone.foodlog.config;
+package dku.capstone.foodlog.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;

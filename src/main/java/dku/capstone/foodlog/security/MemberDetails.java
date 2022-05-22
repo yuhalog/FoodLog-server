ce12e2e814810a01e09224dae8eba14fbd078bfb
YuHayeon <youha0930@gmail.com>
2022-05-23 01:20:48 +0900

    feat: 시큐리티 적용 및 jwt 필터 추가

---
+++ src/main/java/dku/capstone/foodlog/security/MemberDetails.java
@@ -0,0 +1,51 @@
+package dku.capstone.foodlog.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class MemberDetails implements UserDetails {
+
+    private String email;
+
+    public MemberDetails(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/dku/capstone/foodlog/security/MemberDetailsService.java
@@ -0,0 +1,26 @@
+package dku.capstone.foodlog.security;
+
+import dku.capstone.foodlog.domain.Member;
+import dku.capstone.foodlog.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(email);
+
+        return new MemberDetails(member.getEmail());
+    }
+}

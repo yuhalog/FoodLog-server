01391b641d99f562849068dd5c466fc3708f2d62
YuHayeon <youha0930@gmail.com>
2022-05-24 00:52:15 +0900

    chore: JsonProperty 어노테이션 삭제

--- src/main/java/dku/capstone/foodlog/dto/response/MemberDto.java
+++ src/main/java/dku/capstone/foodlog/dto/response/MemberDto.java
@@ -1,28 +1,20 @@
 package dku.capstone.foodlog.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import dku.capstone.foodlog.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
-@Getter
 @Data
 public class MemberDto {
 
-    @JsonProperty(value = "member_id")
     private Long memberId;
     private String email;
     private String username;
     private LocalDate birthday;
-
-    @JsonProperty(value = "self_bio")
     private String selfBio;
-
-    @JsonProperty(value = "profile_picture")
     private String profilePicture;
 
     public MemberDto(Member member) {

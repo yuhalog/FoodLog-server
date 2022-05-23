a007e0181b0c327383811ec6fc2b36a65f740fc2
kys0411 <32180846@dankook.ac.kr>
2022-05-24 01:43:31 +0900

    feat : post Create 기능 추가

--- src/main/java/dku/capstone/foodlog/api/PostController.java
+++ src/main/java/dku/capstone/foodlog/api/PostController.java
@@ -1,15 +1,13 @@
 package dku.capstone.foodlog.api;
 
+import dku.capstone.foodlog.domain.Post;
 import dku.capstone.foodlog.dto.request.PostFormDto;
 import dku.capstone.foodlog.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
@@ -21,6 +19,20 @@ public class PostController {
 
     @PostMapping("/post/new")
     public PostFormDto newPost(@RequestBody PostFormDto postFormDto){
+
+        Post post = Post.builder()
+                .member(null) //.member(memberRepository.findByUsername(postFormDto.getUsername()))
+                .pictureList(postFormDto.getPictureList())
+                .review(postFormDto.getReview())
+                .type(postFormDto.getType())
+                .purpose(postFormDto.getPurpose())
+                .place(postFormDto.getPlace())
+                .date(postFormDto.getDate())
+                .build()
+                ;
+
+        postService.savePost(post);
+
         log.info("rating={}, review={}, type={}, purpose={}",
                 //postFormDto.getPictureList(),
                 postFormDto.getRating(),
@@ -31,5 +43,19 @@ public class PostController {
                 //postFormDto.getDate());
 
         return postFormDto;
+    }
+    @GetMapping("/post/{postId}")
+    public String seePost(@RequestBody PostFormDto postFormDto){
+        return "ok";
+    }
+
+    @PatchMapping("/post/{postId}/edit")
+    public String editPost(@RequestBody PostFormDto postFormDto){
+        return "ok";
+    }
+
+    @DeleteMapping("/post/{postId}/delete")
+    public String deletePost(@RequestBody PostFormDto postFormDto){
+        return "ok";
     }
 }

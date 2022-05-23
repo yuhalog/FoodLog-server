8fccea7392002a361f373818304be80a5c1b12a1
YuHayeon <youha0930@gmail.com>
2022-05-24 00:55:11 +0900

    chore: lombok 어노테이션 추가

--- src/main/java/dku/capstone/foodlog/dto/request/SaveOrUpdateProfileRequest.java
+++ src/main/java/dku/capstone/foodlog/dto/request/SaveOrUpdateProfileRequest.java
@@ -1,10 +1,14 @@
 package dku.capstone.foodlog.dto.request;
 
 import dku.capstone.foodlog.constant.Gender;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class SaveOrUpdateProfileRequest {
 

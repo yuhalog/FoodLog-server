06b57a908383b70f2cddddb789a467ca2948b251
kys0411 <32180846@dankook.ac.kr>
2022-05-25 02:25:01 +0900

    feat : edit post 기능 추가

--- src/main/java/dku/capstone/foodlog/service/PostService.java
+++ src/main/java/dku/capstone/foodlog/service/PostService.java
@@ -1,8 +1,10 @@
 package dku.capstone.foodlog.service;
 
 import dku.capstone.foodlog.domain.Post;
+import dku.capstone.foodlog.dto.request.PostReviewOnly;
 import dku.capstone.foodlog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,20 +13,33 @@ import java.util.Optional;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
     private final PostRepository postRepository;
 
+    //create post
     public Post createPost(Post post){
         return postRepository.save(post);
     }
 
-    public Optional<Post> seePost(Long postId){
-        Optional<Post> post = postRepository.findById(postId);
+    //see post
+    public Post seePost(Long postId){
+        Post post = postRepository.getById(postId);
         return post;
     }
-    public String editPost(Post post){
+
+    //edit post
+    public String editPost(PostReviewOnly postReviewOnly, Long postId){
+        Post findPost = postRepository.getById(postId);
+
+        findPost.setReview(postReviewOnly.getReview());
+
+        postRepository.save(findPost);
+
         return "";
     }
+
+    //delete post
     public void deletePost(Long postId){
         postRepository.deleteById(postId);
     }

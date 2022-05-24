72377fe7489896d50cb8d53e1971ce08a90d96d7
kys0411 <32180846@dankook.ac.kr>
2022-05-24 14:31:18 +0900

    feat : post delete 매핑 추가

--- src/main/java/dku/capstone/foodlog/api/PostController.java
+++ src/main/java/dku/capstone/foodlog/api/PostController.java
@@ -2,6 +2,7 @@ package dku.capstone.foodlog.api;
 
 import dku.capstone.foodlog.domain.Post;
 import dku.capstone.foodlog.dto.request.PostFormDto;
+import dku.capstone.foodlog.repository.PostRepository;
 import dku.capstone.foodlog.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,6 +17,9 @@ public class PostController {
 
     @Autowired
     PostService postService;
+
+    @Autowired
+    PostRepository postRepository;
 
     @PostMapping("/new")
     public PostFormDto newPost(@RequestBody PostFormDto postFormDto){
@@ -46,17 +50,18 @@ public class PostController {
         return postFormDto;
     }
     @GetMapping("/{postId}")
-    public String seePost(@PathVariable String postId){
+    public String seePost(@PathVariable Long postId){
         return "ok";
     }
 
     @PatchMapping("/{postId}/edit")
-    public String editPost(@RequestBody PostFormDto postFormDto){
+    public String editPost(@PathVariable Long postId){
         return "ok";
     }
 
     @DeleteMapping("/{postId}/delete")
-    public String deletePost(@RequestBody PostFormDto postFormDto){
+    public String deletePost(@PathVariable Long postId){
+        postService.deletePost(postId);
         return "ok";
     }
 }

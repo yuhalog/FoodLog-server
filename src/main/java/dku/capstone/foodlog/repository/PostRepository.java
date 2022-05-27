89dc4575dbcee1c8b8cbfd5ea07ee508e1dae2d6
kys0411 <32180846@dankook.ac.kr>
2022-05-27 17:45:57 +0900

    add : getById 메서드 추가

--- src/main/java/dku/capstone/foodlog/repository/PostRepository.java
+++ src/main/java/dku/capstone/foodlog/repository/PostRepository.java
@@ -4,5 +4,6 @@ import dku.capstone.foodlog.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Post getById(Long postId);
 
 }

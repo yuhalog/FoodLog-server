27c6cf351d3abae48d4e2ff4265077944c294b74
kys0411 <32180846@dankook.ac.kr>
2022-05-29 16:45:32 +0900

    add : place 객체를 저장하기 위한 repository 추가

---
+++ src/main/java/dku/capstone/foodlog/repository/PlaceRepository.java
@@ -0,0 +1,7 @@
+package dku.capstone.foodlog.repository;
+
+import dku.capstone.foodlog.domain.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+}

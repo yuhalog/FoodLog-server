2cbef7c33a2110b8528a76dcb0e068009080c655
YuHayeon <youha0930@gmail.com>
2022-06-03 01:50:35 +0900

    feat: 중간 게시물 조회 기능 추가

---
+++ src/main/java/dku/capstone/foodlog/dto/response/PlacePostResponse.java
@@ -0,0 +1,23 @@
+package dku.capstone.foodlog.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+public class PlacePostResponse {
+
+    private Long id;
+    private String name;
+    private String address;
+    private float rating;
+    private List<PictureReview> pictureReviews;
+
+    @Builder
+    public class PictureReview {
+        private String pictureUrl;
+        private String review;
+    }
+}

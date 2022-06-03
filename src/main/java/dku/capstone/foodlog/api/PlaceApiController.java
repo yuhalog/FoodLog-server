bae1620c325524a77d7c12ffe55a7a49e2272ae5
YuHayeon <youha0930@gmail.com>
2022-06-04 01:16:25 +0900

    refactor: 중간게시물 response에 좌표 위치 추가

--- src/main/java/dku/capstone/foodlog/api/PlaceApiController.java
+++ src/main/java/dku/capstone/foodlog/api/PlaceApiController.java
@@ -1,7 +1,5 @@
 package dku.capstone.foodlog.api;
 
-import dku.capstone.foodlog.dto.request.PlaceRequest;
-import dku.capstone.foodlog.dto.request.SaveOrUpdateProfileRequest;
 import dku.capstone.foodlog.dto.response.PlacePostResponse;
 import dku.capstone.foodlog.service.PlaceService;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor

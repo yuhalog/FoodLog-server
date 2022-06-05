5576ae1b9ce5ac52a9665aa4642956f09294e14f
kys0411 <32180846@dankook.ac.kr>
2022-06-05 15:57:57 +0900

    clear git cache

--- src/main/java/dku/capstone/foodlog/domain/Post.java
+++ src/main/java/dku/capstone/foodlog/domain/Post.java
@@ -53,7 +53,7 @@ public class Post extends BaseTime{
                 String review,
                 FoodType type,
                 FoodPurpose purpose,
-                PlacePost placePost,
+                Place place,
                 Date date ){
         this.member = member;
         this.pictureList = pictureList;
@@ -61,7 +61,7 @@ public class Post extends BaseTime{
         this.review = review;
         this.type = type;
         this.purpose = purpose;
-        this.placePost = placePost;
+        this.place = place;
         this.date = date;
     }
 

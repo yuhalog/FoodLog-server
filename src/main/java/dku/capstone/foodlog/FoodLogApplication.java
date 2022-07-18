8fbe058b64bdc34ac43f93f2e1d804fa3db6bc56
YuHayeon <youha0930@gmail.com>
2022-07-19 00:35:29 +0900

    remove: 사용하지 않는 코드 삭제

--- src/main/java/dku/capstone/foodlog/FoodLogApplication.java
+++ src/main/java/dku/capstone/foodlog/FoodLogApplication.java
@@ -2,7 +2,6 @@ package dku.capstone.foodlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 
 @SpringBootApplication
 public class FoodLogApplication {

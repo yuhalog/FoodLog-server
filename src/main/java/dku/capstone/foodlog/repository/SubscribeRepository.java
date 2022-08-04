266477e71142a2a570e13c591558c09262a1ce19
YuHayeon <youha0930@gmail.com>
2022-08-04 20:12:00 +0900

    feat: 구독 리스트 조회 기능 추가

--- src/main/java/dku/capstone/foodlog/repository/SubscribeRepository.java
+++ src/main/java/dku/capstone/foodlog/repository/SubscribeRepository.java
@@ -1,9 +1,16 @@
 package dku.capstone.foodlog.repository;
 
+import dku.capstone.foodlog.domain.Member;
 import dku.capstone.foodlog.domain.Subscribe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+    Subscribe findByMemberAndSubscriber(Member member, Member Subscribe);
+
+    Page<Subscribe> findByMember(Member member, Pageable pageable);
 }

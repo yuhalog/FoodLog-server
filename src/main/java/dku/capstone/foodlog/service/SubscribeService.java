bd8b9ff2e2b43bb8b9ed2d6c39ef2ca3765e7917
YuHayeon <youha0930@gmail.com>
2022-08-04 20:12:00 +0900

    feat: 구독 리스트 조회 기능 추가

--- src/main/java/dku/capstone/foodlog/service/SubscribeService.java
+++ src/main/java/dku/capstone/foodlog/service/SubscribeService.java
@@ -3,9 +3,12 @@ package dku.capstone.foodlog.service;
 import dku.capstone.foodlog.domain.Member;
 import dku.capstone.foodlog.domain.Subscribe;
 import dku.capstone.foodlog.dto.request.SubscribeRequest;
+import dku.capstone.foodlog.dto.response.SubscribeListResponse;
 import dku.capstone.foodlog.repository.MemberRepository;
 import dku.capstone.foodlog.repository.SubscribeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,5 +54,15 @@ public class SubscribeService {
         member.getSubscribers().remove(subscribe);
 
         return subscribe.getId();
+    }
+
+    /**
+     * 구독 리스트 조회
+     */
+    public Page<SubscribeListResponse> getSubscribeList (Pageable pageable, Member member) {
+        Page<Subscribe> subscribers = subscribeRepository.findByMember(member, pageable);
+        Page<SubscribeListResponse> subscribeList = subscribers.map(entity -> new SubscribeListResponse(entity));
+
+        return subscribeList;
     }
 }

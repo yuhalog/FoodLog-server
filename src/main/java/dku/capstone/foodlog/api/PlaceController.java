baffe9116e8831635effce2388653fe66ff099da
YuHayeon <youha0930@gmail.com>
2022-09-08 11:02:06 +0900

    fix: controller 이름 변경

--- src/main/java/dku/capstone/foodlog/api/PlaceController.java
+++ src/main/java/dku/capstone/foodlog/api/PlaceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public class PlaceApiController {
+public class PlaceController {
 
     private final PlaceService placeService;
 

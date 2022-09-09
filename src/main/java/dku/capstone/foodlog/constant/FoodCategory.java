8095e70e86f60feb6c1e7a0a3ca8091f348eb9f6
YuHayeon <youha0930@gmail.com>
2022-09-10 02:01:21 +0900

    fix: Category 변경

--- src/main/java/dku/capstone/foodlog/constant/FoodCategory.java
+++ src/main/java/dku/capstone/foodlog/constant/FoodCategory.java
@@ -1,12 +1,34 @@
 package dku.capstone.foodlog.constant;
 
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum FoodCategory {
-    KOREAN,
-    CHINESE,
-    JAPANESE,
-    WESTERN,
-    ASIAN,
-    DESSERT,
-    SNACK,
-    CHICKEN
+    KOREAN("한식"),
+    CHINESE("중식"),
+    JAPANESE("일식"),
+    WESTERN("양식"),
+    ASIAN("아시아음식"),
+    DESSERT("간식"),
+    CAFE("카페"),
+    SNACK("분식"),
+    CHICKEN("치킨"),
+    ETC("기타");
+
+    @Getter
+    private final String value;
+
+    FoodCategory(String value) {
+        this.value = value;
+    }
+
+    private static final Map<String, FoodCategory> findByValue =
+            Stream.of(values()).collect(Collectors.toMap(FoodCategory::getValue, e -> e));
+
+    public static FoodCategory valueOfFoodCategory(String value) {
+        return findByValue.get(value);
+    }
 }

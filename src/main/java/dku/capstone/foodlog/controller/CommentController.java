641384e584ae18f7c9f1b2e719f88606a79f9912
YuHayeon <youha0930@gmail.com>
2022-09-15 10:33:27 +0900

    feat: 댓글 생성 및 삭제

--- src/main/java/dku/capstone/foodlog/controller/CommentController.java
+++ src/main/java/dku/capstone/foodlog/controller/CommentController.java
@@ -1,25 +1,35 @@
 package dku.capstone.foodlog.controller;
 
-import dku.capstone.foodlog.dto.request.CommentDto;
+import dku.capstone.foodlog.domain.Member;
+import dku.capstone.foodlog.dto.CommentDto;
 import dku.capstone.foodlog.service.CommentService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/comment")
+@RequestMapping("/api")
+@RequiredArgsConstructor
 @RestController
 public class CommentController {
 
-    @Autowired
-    CommentService commentService;
+    private final CommentService commentService;
 
-    @PostMapping("/{postId}/new")
-    public CommentDto newComment(@PathVariable Long postId, @RequestPart CommentDto commentDto){
-        CommentDto comment = commentService.createComment(postId, commentDto);
-        return comment;
+    @PostMapping("/v1/post/{postId}/comment")
+    public ResponseEntity<CommentDto.Response> newComment(
+            @PathVariable("postId") Long postId,
+            @RequestBody CommentDto.Request commentRequest,
+            @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : member") Member member) {
+        CommentDto.Response commentResponse = commentService.createComment(postId, commentRequest, member);
+        return new ResponseEntity<>(commentResponse, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{postId}/{commentId}")
-    public void deleteComment(@PathVariable Long postId, @PathVariable Long commentId){
+    @DeleteMapping("/v1/post/{postId}/comment/{commentId}")
+    public ResponseEntity<?> deleteComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId) {
         commentService.deleteComment(postId, commentId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/dku/capstone/foodlog/service/CommentService.java
+++ src/main/java/dku/capstone/foodlog/service/CommentService.java
@@ -1,51 +1,11 @@
 package dku.capstone.foodlog.service;
 
-import dku.capstone.foodlog.domain.Comment;
 import dku.capstone.foodlog.domain.Member;
-import dku.capstone.foodlog.domain.Post;
-import dku.capstone.foodlog.dto.request.CommentDto;
-import dku.capstone.foodlog.repository.CommentRepository;
-import dku.capstone.foodlog.repository.MemberRepository;
-import dku.capstone.foodlog.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import dku.capstone.foodlog.dto.CommentDto;
 
-@Service
-public class CommentService {
+public interface CommentService {
 
-    @Autowired
-    CommentRepository commentRepository;
+    void deleteComment(Long postId, Long commentId);
 
-    @Autowired
-    PostRepository postRepository;
-
-    @Autowired
-    MemberRepository memberRepository;
-
-    public CommentDto createComment(Long postId, CommentDto commentDto){
-        Post post = postRepository.findById(postId).get();
-        Member member = memberRepository.findById(commentDto.getMemberId()).get();
-        Comment comment = Comment.builder()
-                .post(post)
-                .member(member)
-                .content(commentDto.getContent())
-                .build();
-
-        post.getCommentList().add(comment);
-        postRepository.save(post);
-
-        commentRepository.save(comment);
-
-        return commentDto;
-    }
-
-    public void deleteComment(Long postId, Long commentId){
-        Post post = postRepository.findById(postId).get();
-        Comment comment = commentRepository.findById(commentId).get();
-
-        post.getCommentList().remove(comment);
-        postRepository.save(post);
-
-        commentRepository.deleteById(commentId);
-    }
+    CommentDto.Response createComment(Long postId, CommentDto.Request commentRequest, Member member);
 }

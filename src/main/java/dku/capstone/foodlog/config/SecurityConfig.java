a7a43f9cc3f1ad89afaaedab3bd4fc754dccb6c0
YuHayeon <youha0930@gmail.com>
2022-09-15 10:36:35 +0900

    fix: SecurityConfig url 수정

--- src/main/java/dku/capstone/foodlog/config/SecurityConfig.java
+++ src/main/java/dku/capstone/foodlog/config/SecurityConfig.java
@@ -33,9 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/api/member/login").permitAll()
-                .antMatchers("/api/member/join").permitAll()
-                .antMatchers("/post/**").permitAll()
+                .antMatchers("/api/v1/member/login").permitAll()
+                .antMatchers("/api/v1/member/join").permitAll()
                 .antMatchers("/s3/**").permitAll()
                 .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated();

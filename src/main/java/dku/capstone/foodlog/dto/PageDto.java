c1874e49f93f7ba2ebef7b2cd8fdb33445826656
YuHayeon <youha0930@gmail.com>
2022-09-16 10:17:06 +0900

    add: PageDto 추가

---
+++ src/main/java/dku/capstone/foodlog/dto/PageDto.java
@@ -0,0 +1,31 @@
+package dku.capstone.foodlog.dto;
+
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PageDto<ENTITY, DTO> {
+
+    private List<DTO> content;
+
+    private Integer pageNumber;
+
+    private Integer pageSize;
+
+    private Long totalElements;
+
+    private Integer totalPages;
+
+    private boolean isLast;
+
+    public PageDto(Page<ENTITY> page, Function<ENTITY,DTO> fn) {
+        this.content = page.getContent().stream().map(fn).collect(Collectors.toList());;
+        this.pageNumber = page.getNumber();
+        this.pageSize = page.getNumberOfElements();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.isLast = ((page.getTotalPages()-1)==page.getNumber());
+    }
+}

35fbd15c6a016b7f4d775aeb05ed4770f7b58eca
YuHayeon <youha0930@gmail.com>
2022-09-19 21:43:02 +0900

    fix: 데이터 return 안되는 문제 해결
    PageDto에 Getter가 없기 때문에 데이터 오류났었음.
    추가완료 후 정상적인 작동 확인

--- src/main/java/dku/capstone/foodlog/controller/PlaceController.java
+++ src/main/java/dku/capstone/foodlog/controller/PlaceController.java
@@ -1,14 +1,16 @@
 package dku.capstone.foodlog.controller;
 
 import dku.capstone.foodlog.dto.MapDto;
+import dku.capstone.foodlog.dto.PageDto;
+import dku.capstone.foodlog.dto.PlaceDto;
 import dku.capstone.foodlog.service.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +23,22 @@ public class PlaceController {
 
     @PostMapping("/v1/map")
     public ResponseEntity<?> getPlaceWithFilter(
-            @RequestBody MapDto.Request mapRequest) {
-        List<MapDto.Response> responses = placeService.searchPlaceWithFilter(mapRequest);
+            @RequestBody MapDto.Filter mapFilterRequest) {
+        List<MapDto.Response> responses = placeService.searchPlaceWithFilter(mapFilterRequest);
         return new ResponseEntity<>(responses, HttpStatus.OK);
+    }
+
+    @GetMapping("/v1/place/search/name")
+    public PageDto searchPlaceByName(
+            @RequestParam(defaultValue = "") String query,
+            @RequestParam(defaultValue = "") Double latitude,
+            @RequestParam(defaultValue = "") Double latitudeDelta,
+            @RequestParam(defaultValue = "") Double longitude,
+            @RequestParam(defaultValue = "") Double longitudeDelta,
+            @PageableDefault(size=10, sort = "placeId", direction = Sort.Direction.DESC) Pageable pageable) {
+        MapDto.Search searchRequest = new MapDto.Search(longitude, latitude, longitudeDelta, latitudeDelta, query);
+        PageDto placeResponsePageDto = placeService.searchPlaceByName(searchRequest, pageable);
+
+        return placeResponsePageDto;
     }
 }
